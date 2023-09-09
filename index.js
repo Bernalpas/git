32900fe049b9bcb6efb762cfb42eeee74357041d
Berni <bernalpas@gmail.com>
2023-09-09 11:24:02 -0300

    Inicio proyecto de ecommerce

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Hola mundo de Git');
+console.log('Hola mundo de Git');
+console.log('Hola mundo de Git');
+
+let nombre = 'Berni';
+let provincia = 'Mendoza';
+let pais = 'Argentina';
+let fecha = 2023
+
+console.log('Buenas, mi nombre es ' + nombre + ' y vivo en la provincia de ' + provincia + ' que se encuentra en ' + pais);
+
+
