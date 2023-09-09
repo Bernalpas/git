08c0d8fb96824b64411898a57d1ddd8731f6b290
Berni <bernalpas@gmail.com>
2023-09-09 12:57:37 -0300

    creamos una función

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ let fecha = 2023
 console.log('Buenas, mi nombre es ' + nombre + ' y vivo en la provincia de ' + provincia + ' que se encuentra en ' + pais);
 
 
+console.log('=============================================');
+
+function sumar() {
+    
+    let numeroUno = 55000
+    let numeroDos = 89453;
+    
+    let resultado = numeroUno + numeroDos;
+    
+    console.log('=============================================')
+    console.log(`El resultado de la suma es ${resultado}`);
+    console.log('=============================================')
+    console.log("El resultado de la suma es " + resultado);
+    console.log('=============================================');
+    console.log('El resultado de la suma es ' + resultado);
+    console.log('=============================================');
+}
+
+sumar();
+
+
